bc4ff85843e2cf365c230e414fb085e9e98107b8
Girish <pgirisha77@gmail.com>
2019-12-15 19:04:29 +0530

    Added junit Support and test cases

---
+++ src/test/java/com/example/girish/main/entities/CategoryTest.java
@@ -0,0 +1,24 @@
+package com.example.girish.main.entities;
+
+import com.example.girish.main.entity.Category;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    private Category category;
+
+    @Before
+    public void setTup() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long id = 4L;
+        category.setId(id);
+        assertEquals(id, category.getId());
+    }
+}
---
+++ src/test/java/com/example/girish/main/service/RecipeServiceTest.java
@@ -0,0 +1,5 @@
+package com.example.girish.main.service;
+
+
+public class RecipeServiceTest {
+}
