8f220724a5bcaa025d6837b7be4dd36f4516de3c
Girish <pgirisha77@gmail.com>
2019-12-15 20:26:10 +0530

    Change in test class

--- src/main/java/com/example/girish/main/utility/Starter.java
+++ src/main/java/com/example/girish/main/utility/Starter.java
@@ -1,4 +1,4 @@
-package com.example.girish.main.bootstrap.utility;
+package com.example.girish.main.utility;
 
 import com.example.girish.main.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
